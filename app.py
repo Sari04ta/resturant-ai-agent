0be2269fb36eb4e9d0365684e4e7273c42f66adb
Sarita Mardi <155515140+Sari04ta@users.noreply.github.com>
2025-12-23 19:04:23 +0530

    Add Streamlit configuration to app.py

--- app.py
+++ app.py
@@ -12,8 +12,9 @@ from utils.analysis_utils import (
     get_price_view,
     get_menu_popularity_view,
 )
-
 from agent_code import build_context, run_agent
+
+
 
 
 # --------------------------------------------------
